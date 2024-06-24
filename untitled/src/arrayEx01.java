a170d12de70a2a641982dca587651b565f69a724
이여원 <127466476+33ueowon@users.noreply.github.com>
2024-06-25 04:17:28 +0900

    Add files via upload

---
+++ untitled/src/arrayEx01.java
@@ -0,0 +1,7 @@
+public class arrayEx01 {
+    public static void main(String[] args) {
+        int[] a = new int [3]; //배열 생성
+        a[0] =10; a[1] = 20; a[2] = 30; //배열 선언
+        System.out.println(a.length);   // 배열의 길이 호출
+    }
+}
---
+++ untitled/src/castingEx01.java
@@ -0,0 +1,6 @@
+public class castingEx01 {
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = a;
+    }
+}
