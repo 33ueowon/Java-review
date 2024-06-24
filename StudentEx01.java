b71b9f463b89d172e9790b7372ca29e6ab7dc3e1
이여원 <127466476+33ueowon@users.noreply.github.com>
2024-06-25 04:16:47 +0900

    2024.06.25

---
+++ StudentEx01.java
@@ -0,0 +1,12 @@
+class Student {
+    String name;
+    int num;
+    int phonenum;
+    String adress;
+    String subject;
+}
+class Leader extends Student{
+    String leader;
+}
+public class StudentEx01 {
+}
