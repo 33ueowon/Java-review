c48572ad87e18bb1718982caa6f13aa29d4cbb40
이여원 <127466476+33ueowon@users.noreply.github.com>
2024-06-25 04:18:27 +0900

    Delete arrayEx02.java

--- arrayEx02.java
+++
@@ -1,8 +0,0 @@
-public class arrayEx02 {
-    public static void main(String[] args) {
-        int[] a = new int[]{1,2,3,4,5,6,7,8,9,10}; //배열 생성
-        for (int i = 0; i < a.length; i++){ // a의 배열의 길이 만큼 돌린다.
-            System.out.println("a["+ i +"] = " + a[i]);
-        }
-    }
-}
